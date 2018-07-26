729d8b12bce33ebc0dbf4612cd839fde3fe2a25e
Ben Wood <bwood@wheelercat.com>
2018-07-26 10:59:12 -0600

    Adds delete from cart method to controller

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -44,9 +44,12 @@ public class CartController extends HttpServlet {
 			switch(action) {
 				case "/addcart":
 					 addToCart(request, response);
-           break;
-        default:
-           break;
+					 break;
+				case "/delete":
+					deleteFromCart(request, response);
+					break;           
+				default:
+					break;
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -81,6 +84,10 @@ public class CartController extends HttpServlet {
 
 	 // Add this item and quantity to the ShoppingCart
    shoppingCart.addCartItem(existingBook, quantity);
+  }
+  
+  private void deleteFromCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	  
   }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
